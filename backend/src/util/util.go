ef0a4d51ec927b4a03a325e00d0ae879d99bf3d7
Opisek <darowski.kacper@gmail.com>
2024-09-09 14:00:23 +0200

    remove outdated code

--- backend/src/util/util.go
+++ backend/src/util/util.go
@@ -1,22 +1,5 @@
 package util
 
-import (
-	"luna-backend/sources"
-	"luna-backend/types"
-)
-
-// TODO: this will become better and will use a map once there is user logic and persistance
-var Sources []sources.Source
-
-func GetSource(id types.ID) sources.Source {
-	for _, source := range Sources {
-		if source.GetId() == id {
-			return source
-		}
-	}
-	return nil
-}
-
 func Filter[T any](ss []T, test func(T) bool) (ret []T) {
 	for _, s := range ss {
 		if test(s) {
