d6442a7ea6b7fe5c69e19e55003ef6a3a8fbbf9a
Opisek <darowski.kacper@gmail.com>
2024-09-14 12:23:22 +0200

    concept for event dates

---
+++ backend/src/types/date.go
@@ -0,0 +1,73 @@
+package types
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type EventDate struct {
+	start           time.Time
+	end             time.Time
+	duration        time.Duration
+	specifyDuration bool
+	allDay          bool
+	recurrence      EventRecurrence
+}
+
+type eventDateMarshalEnd struct {
+	Start      time.Time       `json:"start"`
+	End        time.Time       `json:"end"`
+	AllDay     bool            `json:"allDay"`
+	Recurrence EventRecurrence `json:"recurrence"`
+}
+
+type eventDateMarshalDuration struct {
+	Start      time.Time       `json:"start"`
+	Duration   time.Duration   `json:"duration"`
+	AllDay     bool            `json:"allDay"`
+	Recurrence EventRecurrence `json:"recurrence"`
+}
+
+func (ed *EventDate) MarshalJSON() ([]byte, error) {
+	if ed.specifyDuration {
+		return json.Marshal(eventDateMarshalDuration{
+			Start:      ed.start,
+			Duration:   ed.duration,
+			AllDay:     ed.allDay,
+			Recurrence: ed.recurrence,
+		})
+	} else {
+		return json.Marshal(eventDateMarshalEnd{
+			Start:      ed.start,
+			End:        ed.end,
+			AllDay:     ed.allDay,
+			Recurrence: ed.recurrence,
+		})
+	}
+}
+
+func (ed *EventDate) UnmarshalJSON(data []byte) error {
+	var edme eventDateMarshalEnd
+	if err := json.Unmarshal(data, &edme); err == nil {
+		ed.start = edme.Start
+		ed.end = edme.End
+		ed.allDay = edme.AllDay
+		ed.recurrence = edme.Recurrence
+		ed.specifyDuration = false
+		return nil
+	}
+
+	var edmd eventDateMarshalDuration
+	if err := json.Unmarshal(data, &edmd); err == nil {
+		ed.start = edmd.Start
+		ed.duration = edmd.Duration
+		ed.allDay = edmd.AllDay
+		ed.recurrence = edmd.Recurrence
+		ed.specifyDuration = true
+		return nil
+	}
+
+	return nil
+}
+
+type EventRecurrence map[string]string
