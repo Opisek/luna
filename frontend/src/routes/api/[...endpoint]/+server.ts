d36746e017cca8828ef66ae5c872112cdd7d6a80
Opisek <darowski.kacper@gmail.com>
2024-09-15 14:30:26 +0200

    nginx tests

--- frontend/src/routes/api/[...endpoint]/+server.ts
+++ frontend/src/routes/api/[...endpoint]/+server.ts
@@ -2,7 +2,6 @@ import type { RequestEvent } from "./$types";
 //import { API_URL } from "$env/static/private";
 
 const proxy = (async ({ params, request }: RequestEvent) => {
-  console.log(request.url)
   // @ts-ignore
   return await fetch(`${process.env.API_URL}/api/${params.endpoint}`, request);
 })
