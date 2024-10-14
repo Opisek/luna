f42827340d0fe4a51f6f7fb52302dc3aa2ec8873
Opisek <darowski.kacper@gmail.com>
2024-10-14 21:15:47 +0200

    reduce amount of calculations

--- frontend/src/lib/common/comparators.ts
+++ frontend/src/lib/common/comparators.ts
@@ -1,6 +1,7 @@
 export function compareEventsByStartDate(a: EventModel, b: EventModel): number {
-  if (a.date.start === b.date.start) {
+  const diff = a.date.start.getTime() - b.date.start.getTime();
+  if (diff === 0) {
     return a.date.allDay ? b.date.allDay ? 0 : 1 : b.date.allDay ? -1 : 0;
   }
-  return a.date.start.getTime() - b.date.start.getTime();
+  return diff;
 }
