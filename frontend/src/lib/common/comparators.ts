b6fa965ee40e7b7c7fc5fe659d4942d858ab9e30
Opisek <darowski.kacper@gmail.com>
2024-10-14 20:22:23 +0200

    always put all-day events first

--- frontend/src/lib/common/comparators.ts
+++ frontend/src/lib/common/comparators.ts
@@ -1,3 +1,6 @@
 export function compareEventsByStartDate(a: EventModel, b: EventModel): number {
+  if (a.date.start === b.date.start) {
+    return a.date.allDay ? b.date.allDay ? 0 : 1 : b.date.allDay ? -1 : 0;
+  }
   return a.date.start.getTime() - b.date.start.getTime();
 }
