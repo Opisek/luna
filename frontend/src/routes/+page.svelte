82b56132728355565c08c1e562509350a38b31b9
Opisek <darowski.kacper@gmail.com>
2024-10-24 09:04:24 +0200

    remove debug artifact

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -197,7 +197,6 @@
         <PlusIcon/>
       </IconButton>
       <SourceModal bind:showCreateModal={showNewSourceModal} source={newSource}/>
-      <button on:click={() => queueNotification("success", "test")}>test</button>
     </Horizontal>
   </aside>
   <main>
