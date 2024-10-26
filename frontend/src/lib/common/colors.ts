bd38aefd186a4c6fd862c7ddec418b7e6fc44257
Opisek <darowski.kacper@gmail.com>
2024-10-26 22:10:09 +0200

    reduce event hover color change

--- frontend/src/lib/common/colors.ts
+++ frontend/src/lib/common/colors.ts
@@ -126,7 +126,7 @@ export const isValidColor = (color: string | null | undefined) => {
 
 export function calculateSecondaryColor(rgb: [number, number, number]): [number, number, number] {
   const hsl = RGBtoHSL(rgb);
-  isDark(rgb) ? hsl[2] += 15 : hsl[2] -= 15;
+  isDark(rgb) ? hsl[2] += 10 : hsl[2] -= 10;
   return HSLtoRGB(hsl);
 }
 
