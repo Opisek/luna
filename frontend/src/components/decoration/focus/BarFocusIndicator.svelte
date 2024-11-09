eef68a60465892384be961966510210e69f13703
Opisek <darowski.kacper@gmail.com>
2024-11-09 22:37:26 +0100

    don't switch focus colour to blue when removing error

--- frontend/src/components/decoration/focus/BarFocusIndicator.svelte
+++ frontend/src/components/decoration/focus/BarFocusIndicator.svelte
@@ -14,16 +14,19 @@
     left: 0;
     top: 0;
     transform: translateX(-100%);
-    transition: transform $animationSpeedFast linear;
+    transition: transform $animationSpeedFast linear, background-color 0s linear $animationSpeedFast;
   }
 
   :global(*:focus-within:not(.clicked)) > div, 
   :global(*.error) > div {
     transform: translateX(0);
+    transition: transform $animationSpeedFast linear;
   }
 
   :global(*.error:not(:focus-within:not(.clicked))) > div {
     background-color: $backgroundFailure;
+    transition-delay: 0;
+    transition: transform $animationSpeedFast linear;
   }
 </style>
 
