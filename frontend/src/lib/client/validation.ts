80f1c7b9c2897cc1c7eb4c3ac368f1b66e983b57
Opisek <darowski.kacper@gmail.com>
2024-11-10 20:43:53 +0100

    add the same input validation functions to backend

--- frontend/src/lib/client/validation.ts
+++ frontend/src/lib/client/validation.ts
@@ -1,3 +1,4 @@
+const characterRegex = /^[a-zA-Z0-9]*$/;
 const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
@@ -19,8 +20,8 @@ export const isValidUsername: InputValidation = (username) => {
   if (username.length < 3)
     return invalidResponse("Username must be at least 3 characters long.");
   if (username.length > 25)
-    return invalidResponse("Username must be at most 20 characters long.");
-  if (!/^[a-zA-Z0-9]*$/.test(username))
+    return invalidResponse("Username must be at most 25 characters long.");
+  if (!characterRegex.test(username))
     return invalidResponse("Username must only contain letters and numbers.");
   return validResponse;
 }
