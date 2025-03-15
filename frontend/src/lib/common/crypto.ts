8bf05014c45eee2f0ea49780e2b6585d529dad15
Opisek <darowski.kacper@gmail.com>
2025-03-15 15:04:07 +0100

    change hash package to fix failing build

--- frontend/src/lib/common/crypto.ts
+++ frontend/src/lib/common/crypto.ts
@@ -1,5 +1,7 @@
-import hash from "hash.js";
+import sha1 from "sha1";
 
+// Note: The library object-hash is really cool, but it always adds type prefixes, e.g. string:5:hello, which is not what we want.
+// If they ever add an option to disable this, we can consider switching due to its much higher flexibility and configurability.
 export function getSha1Hash(data: string): string {
-  return hash.sha1().update(data).digest("hex");
+  return sha1(data);
 }
