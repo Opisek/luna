6b4ad0b4aa26ea78d29f76183f6d1d53286478a1
Opisek <darowski.kacper@gmail.com>
2025-04-01 17:00:54 +0200

    automatic ripple colors

--- frontend/src/components/decoration/Ripple.svelte
+++ frontend/src/components/decoration/Ripple.svelte
@@ -51,7 +51,6 @@
 
     animation: ripple animations.$animationSpeedVerySlow animations.$cubic forwards;
 
-    background-color: colors.$ripple;
     opacity: 0.5;
     transition: opacity animations.$animationSpeed;
     transform: scale(0);
@@ -62,6 +61,13 @@
   div.ripple.disappear {
     transition: opacity animations.$animationSpeedSlow !important;
     opacity: 0;
+  }
+
+  :global(html[data-theme="light"]) div.ripple {
+    background-color: colors.$foregroundBright;
+  }
+  :global(html[data-theme="dark"]) div.ripple {
+    background-color: colors.$foregroundDark;
   }
 </style>
 
